e39bfe72461e357d73333b86339e78dc8a991f3e
tao009 <artemsatkeev@gmail.com>
2019-11-15 18:25:50 +0600

    fest list

---
+++ templates/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/data')
+def data():
+    return render_template("data.html")
+
+
+@app.route('/mango')
+def mango():
+    return render_template("mango.html")
+
+
+if __name__ == '__main__':
+    app.run()
